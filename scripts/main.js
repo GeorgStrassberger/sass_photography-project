0f4f359cb3036b0faca19bad6570c2eea53071a0
Georg <georg.strassberger@googlemail.com>
2023-04-24 10:40:40 +0200

    3d effect for second offers card

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-function checkScroll(params) {
+function checkScroll() {
 	const videos = document.querySelectorAll(".story__video");
 
 	for (const video of videos) {
@@ -19,3 +19,32 @@ checkScroll();
 window.addEventListener("load", checkScroll, false);
 window.addEventListener("scroll", checkScroll, false);
 window.addEventListener("resize", checkScroll, false);
+
+function addMousePositionToCss() {
+	const elements = document.querySelectorAll(".offers__card-container");
+	for (const element of elements) {
+		element.addEventListener("mousemove", function (e) {
+			var rect = element.getBoundingClientRect();
+			var x = e.clientX - rect.left;
+			var y = e.clientY - rect.top;
+			element.style =
+				"--mouse-x:" +
+				x / element.offsetWidth +
+				";--mouse-y:" +
+				y / element.offsetHeight +
+				";";
+		});
+		element.addEventListener("mouseleave", function (e) {
+			element.style = "";
+		});
+	}
+}
+
+if (
+	document.readyState === "complete" ||
+	document.readyState === "interactive"
+) {
+	setTimeout(addMousePositionToCss, 1);
+} else {
+	document.addEventListener("DOMContentLoaded", addMousePositionToCss, false);
+}

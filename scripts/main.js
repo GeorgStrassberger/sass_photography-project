32e1dec3001048d9177d81f47b6c59668bbcb259
Georg <georg.strassberger@googlemail.com>
2023-04-26 12:46:01 +0200

    booking-modal script to hold posion + responsive

--- scripts/main.js
+++ scripts/main.js
@@ -48,3 +48,25 @@ if (
 } else {
 	document.addEventListener("DOMContentLoaded", addMousePositionToCss, false);
 }
+
+function bookingModalFixClose() {
+	const elements = document.querySelectorAll(".booking-modal__close");
+	for (const element of elements) {
+		element.addEventListener("click", function () {
+			const scrollY = window.scrollY;
+			const scrollX = window.scrollX;
+			setTimeout(function () {
+				window.scrollTo({ top: scrollY, left: scrollX });
+			}, 1);
+		});
+	}
+}
+
+if (
+	document.readyState === "complete" ||
+	document.readyState === "interactive"
+) {
+	setTimeout(bookingModalFixClose, 1);
+} else {
+	document.addEventListener("DOMContentLoaded", bookingModalFixClose, false);
+}
